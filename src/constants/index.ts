d0355d9d3ccc9538f07841ef9002837f878ad8ea
caduceus33 <marcrodriguez93@gmail.com>
2021-03-06 19:45:01 +0100

    router address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@igniswap/sdk'
 
-export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+export const ROUTER_ADDRESS = '0x0c8eFd86cD72311B16cb12E16BDf74CeE1EBe4Be'
 
 // a list of tokens by chain
 type ChainTokenList = {
